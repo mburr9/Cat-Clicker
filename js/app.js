acbf4e6f397caec7665c27c926c2d5082111ddcc
Mark <mburr9@gmail.com>
2018-10-22 16:46:04 -0400

    modified javascript for 5 cats. Created cat constructor and functions.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,56 @@
-let clicks = 0;
-$('#minerva').click(function() {
-  $('.clicks').html(clicks += 1);
-});
-$('#gus').click(function() {
-  $('.clicks').html(clicks += 1);
+currentCat = null;
+
+
+$(function() {
+  for (let i = 0; i < allCats.length; i++) {
+    let elem = `<li class="cat-name">${allCats[i].name}</li>`;
+    $('.cat-list').append(elem);
+  };
 });
 
-let catNames = ['Minerva', 'Gus'];
 $(function() {
-  $('.cat').each(function(i) {
-    $(this).append(`<p>${catNames[i]}<p>`);
+  let i = 0;
+  let cats = $('.cat-name');
+  cats.each(function() {
+    $(this).click( (function(index) {
+      return function() {
+        currentCat = allCats[index];
+        currentCat.placeImage();
+        currentCat.setClicks();
+      };
+    })(i));
+    i += 1;
   });
 });
+
+$('.cat-pic').click(function() {
+  currentCat.clicks += 1;
+  $('.clicks').html(currentCat.clicks);
+});
+
+
+class Cat {
+  constructor(name,url,desc) {
+    this.name = name;
+    this.clicks = 0;
+    this.imageUrl = url;
+    this.desc = desc;
+  }
+
+  placeImage() {
+    $('.cat-pic').attr({
+      src: `images/${this.imageUrl}.jpg`,
+      alt: this.desc
+    });
+  };
+
+  setClicks() {
+    $('.clicks').html(this.clicks);
+  };
+}
+
+let allCats = [ new Cat('Minerva','minerva', 'cat sitting on a pumpkin'),
+                new Cat('Gus', 'gus', 'cat laying outside'),
+                new Cat('Ralph', 'ralph', 'cat laying inside'),
+                new Cat('Reggie', 'reggie', 'cat laying with crazy eyes'),
+                new Cat('Sam', 'sam', 'cat sitting like a human')];
