c97c519733c62bda2571b9ed6711c64686fb8bb4
Mark <mburr9@gmail.com>
2018-10-23 13:22:28 -0400

    reorganized app.js from cat-clicker repo using MV*

--- js/app.js
+++ js/app.js
@@ -1,56 +1,109 @@
-currentCat = null;
+$(function(){
 
-
-$(function() {
-  for (let i = 0; i < allCats.length; i++) {
-    let elem = `<li class="cat-name">${allCats[i].name}</li>`;
-    $('.cat-list').append(elem);
+  const model = {
+    currentCat: null,
+    cats: [
+      {
+        name: 'Minerva',
+        imageUrl: 'minerva',
+        desc: 'cat sitting on a pumpkin',
+        clicks: 0
+      },
+      {
+        name: 'Gus',
+        imageUrl: 'gus',
+        desc: 'cat laying outside',
+        clicks: 0
+      },
+      {
+        name: 'Ralph',
+        imageUrl: 'ralph',
+        desc: 'cat laying inside',
+        clicks: 0
+      },
+      {
+        name: 'Reggie',
+        imageUrl: 'reggie',
+        desc: 'cat laying with crazy eyes',
+        clicks: 0
+      },
+      {
+        name: 'Sam',
+        imageUrl: 'sam',
+        desc: 'cat sitting like a human',
+        clicks: 0
+      }
+    ]
   };
-});
 
-$(function() {
-  let i = 0;
-  let cats = $('.cat-name');
-  cats.each(function() {
-    $(this).click( (function(index) {
-      return function() {
-        currentCat = allCats[index];
-        currentCat.placeImage();
-        currentCat.setClicks();
-      };
-    })(i));
-    i += 1;
-  });
-});
+  const octopus = {
+    init: function() {
+            model.currentCat = model.cats[0];
+            catListView.init();
+            catView.init();
+    },
 
-$('.cat-pic').click(function() {
-  currentCat.clicks += 1;
-  $('.clicks').html(currentCat.clicks);
-});
+    getCats: function() {
+        return model.cats;
+    },
+
+    getCurrentCat: function() {
+      return model.currentCat;
+    },
+
+    setCurrentCat: function(cat) {
+      model.currentCat = cat;
+    },
+
+    changeClicks: function() {
+      model.currentCat.clicks += 1;
+      catView.render();
+    }
 
 
-class Cat {
-  constructor(name,url,desc) {
-    this.name = name;
-    this.clicks = 0;
-    this.imageUrl = url;
-    this.desc = desc;
+
   }
 
-  placeImage() {
-    $('.cat-pic').attr({
-      src: `images/${this.imageUrl}.jpg`,
-      alt: this.desc
-    });
+  const catListView = {
+    init: function() {
+      this.render();
+    },
+
+    render: function() {
+              let allCats = octopus.getCats();
+              for (let i = 0; i < allCats.length; i++) {
+                let elem = document.createElement('li');
+                elem.textContent = allCats[i].name;
+                $('.cat-list').append(elem);
+                $(elem).click( (function(index) {
+                  return function() {
+                    octopus.setCurrentCat(allCats[index]);
+                    catView.render();
+                  };
+                })(i));
+              };
+
+    }
   };
 
-  setClicks() {
-    $('.clicks').html(this.clicks);
-  };
-}
+  const catView = {
+    init: function() {
+      $('.cat-pic').click(function() {
+        octopus.changeClicks();
+      });
+      this.render();
+    },
 
-let allCats = [ new Cat('Minerva','minerva', 'cat sitting on a pumpkin'),
-                new Cat('Gus', 'gus', 'cat laying outside'),
-                new Cat('Ralph', 'ralph', 'cat laying inside'),
-                new Cat('Reggie', 'reggie', 'cat laying with crazy eyes'),
-                new Cat('Sam', 'sam', 'cat sitting like a human')];
+    render: function() {
+              let currentCat = octopus.getCurrentCat();
+              $('.cat-pic').attr({
+                src: `images/${currentCat.imageUrl}.jpg`,
+                alt: currentCat.desc
+              });
+              $('.clicks').html(currentCat.clicks);
+    }
+
+  };
+  octopus.init();
+
+});
